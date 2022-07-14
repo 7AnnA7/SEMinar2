80a1bae07235416518b0e4ba2c96068e95de6eb3
AnnA <anna_elfimova@mail.ru>
2022-07-14 12:36:09 +0300

    сем  выводит случайное трёхзначное число и удаляет его вторую цифру

---
+++ task11_sem/Program.cs
@@ -0,0 +1,7 @@
+﻿// выводит случайное трёхзначное число и удаляет его вторую цифру
+
+int number = new Random (). Next (100, 1000);
+int dijit3 = number%10;
+int dijit1 = number/100;
+int result = dijit1*10 + dijit3;
+Console.WriteLine(result);
