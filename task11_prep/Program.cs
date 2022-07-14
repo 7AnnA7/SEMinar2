9c31ce7940fd9f8b3b0a0352851b8291e216e176
AnnA <anna_elfimova@mail.ru>
2022-07-14 12:32:45 +0300

    создан гитигнор

---
+++ task11_prep/Program.cs
@@ -0,0 +1,7 @@
+﻿// выводит случайное трёхзначное число и удаляет его вторую цифру
+
+int number = new Random (). Next (100, 1000);
+int NumberFirst = number/100;
+int NumberSecond = number%10;
+int Rezult = NumberFirst*10 + NumberSecond;
+Console.WriteLine($"Наше случайное число = {number} , исключая среднюю цифру = {Rezult}");
