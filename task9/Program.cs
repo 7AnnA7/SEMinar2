7b8c1ba78f4af9b1a58e2719520c87d0331de233
AnnA <anna_elfimova@mail.ru>
2022-07-14 12:02:37 +0300

    task 9 sluchaynoye chislo i ego max zifra

---
+++ task9/Program.cs
@@ -0,0 +1,20 @@
+﻿// выводит случайное число из отрезка [10;99] и показывает наибольшую цифру числа
+
+int number = new Random (). Next (10, 100);
+int FirstDigit = number/10;
+
+Console.WriteLine($"Наше случайное число = {number}");
+
+int SecondDigit = number%10;
+
+Console.WriteLine($"Первая цифра = {FirstDigit} , вторая цифра {SecondDigit}");
+
+if (FirstDigit > SecondDigit)
+{
+Console.WriteLine($"Первая цифра числа {number} больше. Это {FirstDigit}");
+}
+
+else
+{
+    Console.WriteLine($"Вторая цифра числа {number} больше. Это {SecondDigit}");
+}
