71677d4c9b0287f8c309f9027732679bf1e7fc16
AnnA <anna_elfimova@mail.ru>
2022-07-15 16:14:22 +0300

    task 16

---
+++ task16/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int squareA = numberA * numberA;
+int squareB = numberB * numberB;
+
+if ( numberA == squareB || numberB == squareA)
+{
+    Console.WriteLine("первое число - квадрат второго или второе число - квадрат первого");
+}
+else
+{
+    Console.WriteLine("первое число - не квадрат второго или второе число - не квадрат первого");
+}
