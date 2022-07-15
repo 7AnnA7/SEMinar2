69f7aadea2ba89f3d7092d5e08d27b208da0a288
AnnA <anna_elfimova@mail.ru>
2022-07-15 15:57:26 +0300

    task 14 sem

---
+++ task14_sem/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+Console.WriteLine("Введите число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+int delenie1 = numberFirst%7;
+int delenie2 = numberFirst%23;
+
+if (delenie1 == 0 && delenie2 == 0)
+{
+    Console.WriteLine("Кратно одновременно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Некратно одновременно 7 и 23");
+}
